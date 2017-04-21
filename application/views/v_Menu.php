8d0f55d5e402f2f05eebf130d6b752b2e947a5db
Gab2015 <024gab@gmail.com>
2017-04-21 14:27:24 -0600

    pie y cabeza en v_Menu,21/04/2017

--- application/views/v_Menu.php
+++ application/views/v_Menu.php
@@ -12,7 +12,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <h1 id="HeadMenu">SISTEMAS C&C</h1>      
+      <!--   <img border="0" src="<?php echo base_url('imagenes/logocyc.jpg'); ?>" width="63" height="75"/>   -->    
       </div>
       <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
         <ul class="nav navbar-nav">
