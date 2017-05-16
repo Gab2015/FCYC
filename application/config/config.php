849a2cc1b28c14fa21c475ff57baf7e5bf0ffb01
Gab2015 <024gab@gmail.com>
2017-05-15 19:13:29 -0600

    configuraciones bd,15/05/2017

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.0.21:8081/FCYC';
-//$config['base_url'] = 'http://localhost/FCYC';
+//$config['base_url'] = 'http://192.168.0.21:8081/FCYC';
+$config['base_url'] = 'http://localhost/FCYC';
 /*
 |--------------------------------------------------------------------------
 | Index File
