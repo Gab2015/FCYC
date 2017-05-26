a35f176db265e113d64d18c68a28ae78bb40354e
Gab2015 <024gab@gmail.com>
2017-05-26 09:06:18 -0600

    v_Factura-V1,26/05/2017

--- application/controllers/c_Factura.php
+++ application/controllers/c_Factura.php
@@ -171,54 +171,6 @@ class c_Factura extends CI_Controller {
 		else{
 			if ($this->input->post('btn_guardar') == "Guardar"){
 				$Factura_result = $this->m_Factura->pc_m_Factura_i($NumDoc,$Cliente,$Nombre,$IdTipo,$Cajero,$Vendedor,$CodVendedor,$FechaContable,$NumCaja,$VentaGravada,$VentaExenta,$VentaNoSujeta,$Iva,$Total,$DocType);
-				if (Empty($Factura_result) == FALSE){ 
-					$usuario   = $this->session->userdata('username');
-					$loginuser = $this->session->userdata('loginuser');
-					if ($loginuser == TRUE){ 
-						$menus = $this->menu->MenuUsuario($usuario);
-						$data['activeTabVenta'] = "";
-						$data['activeTabInventario'] = "";
-						$data['activeTabGUsuario'] = "";
-						foreach ($menus as $fila) {
-							if ($fila['venta'] == 'T')
-							{
-								$this->load->model("m_Menu", "menu");
-								$itemsVenta = $this->menu->MenuVenta();	
-								$this->multi_menu->set_items($itemsVenta);
-								$config["nav_tag_open"]          = '<ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">';		
-								$config["parent_tag_open"]       = '<li class="dropdown-submenu">';			
-								$config["parent_anchor_tag"]     = '<a tabindex="-1" href="%s">%s</a>';	
-								$config["children_tag_open"]     = '<ul class="dropdown-menu">';			
-								$config["item_divider"]          = "<li class='divider'></li>";
-								$this->multi_menu->initialize($config);
-								$data['activeTabVenta'] = $this->multi_menu->render();
-
-							}
-							if ($fila['usuario'] == 'T')
-							{
-								$itemsGUsuario = $this->menu->MenuGUsuario();
-								$this->multi_menu->set_items($itemsGUsuario);
-								$config["nav_tag_open"]          = '<ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">';		
-								$config["parent_tag_open"]       = '<li class="dropdown-submenu">';			
-								$config["parent_anchor_tag"]     = '<a tabindex="-1" href="%s">%s</a>';	
-								$config["children_tag_open"]     = '<ul class="dropdown-menu">';			
-								$config["item_divider"]          = "<li class='divider'></li>";
-								$this->multi_menu->initialize($config);
-								$data['activeTabGUsuario'] = $this->multi_menu->render();
-							}
-							if ($fila['inventario'] == 'T')
-							{
-								$itemsInventario = $this->menu->MenuInventario();
-								$this->multi_menu->set_items($itemsInventario);
-								$config["nav_tag_open"]          = '<ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">';		
-								$config["parent_tag_open"]       = '<li class="dropdown-submenu">';			
-								$config["parent_anchor_tag"]     = '<a tabindex="-1" href="%s">%s</a>';	
-								$config["children_tag_open"]     = '<ul class="dropdown-menu">';			
-								$config["item_divider"]          = "<li class='divider'></li>";
-								$this->multi_menu->initialize($config);
-								$data['activeTabInventario'] = $this->multi_menu->render();
-							}
-						}
 						unset($_POST['txt_Cliente']);
 						unset($_POST['txt_Nombre']);
 						unset($_POST['txt_Cajero']);
@@ -238,28 +190,8 @@ class c_Factura extends CI_Controller {
 						unset($_POST['txt_Iva']);
 						unset($_POST['txt_Total']);
 						unset($_POST['btn_guardar']);
-      //creo el array con datos de configuración para la vista
-						$datos_vista['rs_Factura'] = $this->m_Factura->get_Factura();
-						$datos_vista['rs_DetalleFactura'] = $this->m_Factura->get_DetalleFactura();
-						$datos_articulo['rs_articulo'] = $this->m_Factura->get_DetalleArticulo();
-      //cargo la vista pasando los datos de configuracion
-						$this->load->view('v_Head');
-						$this->load->view('v_Header');
-						$this->load->view("v_Menu",$data);
-						$this->load->view('v_Factura',$datos_vista);
-						$this->load->view("v_Factura_Model",$datos_articulo);
-						$this->load->view('v_Foot');
-					}
-					else{
-						$this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">¡Datos incorrectos!</div>');
-						redirect('c_Login/index');
-					}	
+                        redirect('c_Factura/index');  
 				}
-				else{
-					$this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">¡Datos incorrectos!</div>');
-					redirect('c_Login/index');
-				}
-			}
 			else{
 				$this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">¡Datos incorrectos!</div>');
 				redirect('c_login/index');
