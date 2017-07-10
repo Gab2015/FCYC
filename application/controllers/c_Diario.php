53aa30fc1a72161601db9fd1beead862174e52ab
Gab2015 <024gab@gmail.com>
2017-07-10 17:07:55 -0600

    v_Diario-V4,10/07/2017

--- application/controllers/c_Diario.php
+++ application/controllers/c_Diario.php
@@ -60,11 +60,13 @@ class c_Diario extends CI_Controller {
 			}
       //creo el array con datos de configuración para la vista			
             $Usuariobd['usuario'] = $usuario;
+		    $Tipo = ($usuario == "eportillo" ? 1 : 2);
+            $datos_diario['rs_Diario'] = $this->m_Diario->get_UltimoDiario($Tipo);
       //cargo la vista pasando los datos de configuracion
 			$this->load->view('v_Head');
 			$this->load->view('v_Header',$Usuariobd);
 			$this->load->view("v_Menu",$data);
-			$this->load->view('v_Diario');
+			$this->load->view('v_Diario',$datos_diario);
 			$this->load->view('v_Foot');
 		}
 		else{
@@ -72,5 +74,85 @@ class c_Diario extends CI_Controller {
 			redirect('c_Login/index');
 		}	
 	}
+	function set_Diario(){
+		$NumDoc         = $this->input->post("txt_NumDoc");
+		$Tipo        = $this->input->post("txt_NumCaja");
+		$this->form_validation->set_rules("txt_NumCaja", "Caja", "trim|required");
+	if ($this->form_validation->run() == FALSE){
+		$usuario   = $this->session->userdata('username');
+		$loginuser = $this->session->userdata('loginuser');
+		if ($loginuser == TRUE){ 
+			$menus = $this->menu->MenuUsuario($usuario);
+			$data['activeTabVenta'] = "";
+			$data['activeTabInventario'] = "";
+			$data['activeTabGUsuario'] = "";
+			foreach ($menus as $fila) {
+				if ($fila['venta'] == 'T')
+				{
+					$this->load->model("m_Menu", "menu");
+					$itemsVenta = $this->menu->MenuVenta();	
+					$this->multi_menu->set_items($itemsVenta);
+					$config["nav_tag_open"]          = '<ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">';		
+					$config["parent_tag_open"]       = '<li class="dropdown-submenu">';			
+					$config["parent_anchor_tag"]     = '<a tabindex="-1" href="%s">%s</a>';	
+					$config["children_tag_open"]     = '<ul class="dropdown-menu">';			
+					$config["item_divider"]          = "<li class='divider'></li>";
+					$this->multi_menu->initialize($config);
+					$data['activeTabVenta'] = $this->multi_menu->render();
+					
+				}
+				if ($fila['usuario'] == 'T')
+				{
+					$itemsGUsuario = $this->menu->MenuGUsuario();
+					$this->multi_menu->set_items($itemsGUsuario);
+					$config["nav_tag_open"]          = '<ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">';		
+					$config["parent_tag_open"]       = '<li class="dropdown-submenu">';			
+					$config["parent_anchor_tag"]     = '<a tabindex="-1" href="%s">%s</a>';	
+					$config["children_tag_open"]     = '<ul class="dropdown-menu">';			
+					$config["item_divider"]          = "<li class='divider'></li>";
+					$this->multi_menu->initialize($config);
+					$data['activeTabGUsuario'] = $this->multi_menu->render();
+				}
+				if ($fila['inventario'] == 'T')
+				{
+					$itemsInventario = $this->menu->MenuInventario();
+					$this->multi_menu->set_items($itemsInventario);
+					$config["nav_tag_open"]          = '<ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">';		
+					$config["parent_tag_open"]       = '<li class="dropdown-submenu">';			
+					$config["parent_anchor_tag"]     = '<a tabindex="-1" href="%s">%s</a>';	
+					$config["children_tag_open"]     = '<ul class="dropdown-menu">';			
+					$config["item_divider"]          = "<li class='divider'></li>";
+					$this->multi_menu->initialize($config);
+					$data['activeTabInventario'] = $this->multi_menu->render();
+				}
+			}
+      //creo el array con datos de configuración para la vista			
+            $Usuariobd['usuario'] = $usuario;
+		    $Tipo = ($usuario == "eportillo" ? 1 : 2);
+            $datos_diario['rs_Diario'] = $this->m_Diario->get_UltimoDiario($Tipo);
+      //cargo la vista pasando los datos de configuracion
+			$this->load->view('v_Head');
+			$this->load->view('v_Header',$Usuariobd);
+			$this->load->view("v_Menu",$data);
+			$this->load->view('v_Diario',$datos_diario);
+			$this->load->view('v_Foot');
+		}
+		else{
+			$this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">¡Datos incorrectos!</div>');
+			redirect('c_Login/index');
+		}	
+	}
+			else{
+			if ($this->input->post('btn_generar') == "Generar"){
+				$Factura_result = $this->m_Factura->pc_m_Factura_i($NumDoc,$Cliente,$Nombre,$IdTipo,$Cajero,$Vendedor,$CodVendedor,$FechaContable,$NumCaja,$VentaGravada,$VentaExenta,$VentaNoSujeta,$Iva,$Total,$DocType,$DUI,$TipoPago);
+						unset($_POST['txt_NumCaja']);
+						unset($_POST['txt_IdTipo']);
+				}
+			else{
+				$this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">¡Datos incorrectos!</div>');
+				redirect('c_login/index');
+			}
+		}
+  }
 }
 ?> 
--- application/models/m_Diario.php
+++ application/models/m_Diario.php
@@ -3,5 +3,13 @@ class m_Diario extends CI_Model {
     function __construct(){
         parent::__construct();
     }
+    function get_UltimoDiario($Tipo){
+	//Variables
+        $ssqlNumDoc = "EXECUTE pc_m_UltimoDiario ?";
+        $paramsNumDoc = array($Tipo);
+        $rs_Diario = $this->db->query($ssqlNumDoc,$paramsNumDoc);
+        $rs_Diarios['rs_Diario'] = $rs_Diario->result();
+        return  $rs_Diarios;
+    }
  }
 ?> 
